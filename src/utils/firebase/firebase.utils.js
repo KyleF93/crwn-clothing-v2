d0e65af595804a4649f32e1cee513ee0a3085a1e
Kyle Fox <Kylexz@icloud.com>
2022-07-06 20:36:12 +0100

    added onAuthStateChangedListener and cleaned up sign-in & sign-up forms

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,13 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -56,3 +64,10 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
     return await signInWithEmailAndPassword(auth, email, password)
 }
+
+export const signOutUser = async () => {
+    signOut(auth);
+}
+
+export const onAuthStateChangedListener = (callback) =>
+    onAuthStateChanged(auth, callback);
